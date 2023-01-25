696cee46e4243623faaeab5258408bbe76ca1553
Roger Rosena <rogerrosena@gmail.com>
2023-01-24 19:56:38 -0500

    add bootstrap

---
+++ script.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = Title;
+    this.author = Author;
+    this.pages = Pages;
+    this.read = Read;
+};
+
+//function to add books to liibrary array
+function addBookToLibrary(Title, Author, Pages, Read) {
+    let book = new Book(Title, Author, Pages, Read);
+    myLibrary.push(book);
+    //link to a function to display array (or card with books)
+};
