4fd778d0f5a5ca553011c7ed93cfe1cbf3f08045
Roger Rosena <rogerrosena@gmail.com>
2023-01-26 20:25:09 -0500

    day 1 javascript

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const submitButton = docuemnt.getElementById("submit");
 
 function Book(title, author, pages, read) {
     this.title = Title;
@@ -13,3 +14,18 @@ function addBookToLibrary(Title, Author, Pages, Read) {
     myLibrary.push(book);
     //link to a function to display array (or card with books)
 };
+
+function updateDisplay() {
+    const library = document.querySelector(".library")
+
+    //create element and add book from list
+    myLibrary.forEach((myLibrary) => {
+        
+    });
+};
+
+submitButton.addEventLister("click", e => {
+    e.preventDefault();
+    addBookToLibrary();
+    updateDisplay();
+});
