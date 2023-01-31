0aa7687e1e3b8d0778f17616c6ba2339b0a3dc02
Roger Rosena <rogerrosena@gmail.com>
2023-01-30 21:06:34 -0500

    day 3 with js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myLibrary = [];
-const submitButton = docuemnt.getElementById("submit");
+const submitButton = document.getElementById("submit");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -22,56 +22,76 @@ function addBookToLibrary() {
 
 function updateDisplay() {
     const library = document.querySelector(".library");
+    //to clear the display
+    library.innerHTML = "";
 
     //create element and add book from list
-    myLibrary.forEach((myLibrarys) => {
+    myLibrary.forEach((book) => {
         const book = document.createElement("div");
         book.classList.add("book", "card", "mx-2", "mt-3");
         book.style.width = "18rem";
-        //book.data
-        //library.appendChild(book);
+        book.append(library);
 
         const cardBody = document.createElement("div");
         cardBody.classList.add("card-body");
+        cardBody.append(card);
 
         const titleInfo = document.createElement("h5");
         titleInfo.classList.add("card-title");
-        //titleInfo.textContent = theBook.title;
+        titleInfo.textContent = book.title;
+        titleInfo.append(cardBody);
 
         const authorInfo = document.createElement("p");
         authorInfo.classList.add("class-text");
-        //authorInfo.textContent = theBook.author;
+        authorInfo.textContent = book.author;
+        authorInfo.append(cardBody);
 
         const pageInfo = document.createElement("p");
         pageInfo.classList.add("card-text");
-        //pageInfo.textConent = theBook.pages;
+        pageInfo.textContent = book.pages;
+        pageInfo.append(cardBody);
 
         const formDiv = document.createElement("div");
         formDiv.classList.add("form-check", "form-switch");
+        formDiv.append(cardBody);
 
         const readInput = document.createElement("input");
         readInput.classList.add("form-check-input");
+        readInput.append(formDiv);
         
         const readLabel = document.createElement("label");
         readLabel.classList.add("form-check-label");
+        readLabel.type ="checkbox";
+        if (book.read) readLabel = true;
         readLabel.textContent = "Read";
+        readLabel.append(formDiv);
 
         const removeButton = document.createElement("button");
-        removeButton.classList.add("btn", "btn-primary", "btn-sm")
+        removeButton.classList.add("btn", "btn-danger", "btn-sm")
         removeButton.textContent = "Remove Book";
-        //don't forget to append the button to parent (and other elements)
+        removeButton.append(cardBody);
 
-        removeButton.addEventListener("click", removeBookFromLibrary);
-
-        function removeBookFromLibrary () {
-            //function to remove book
-        }
+        removeButton.addEventListener("click", (e) => removeBook(e));
 
     });
 };
 
-submitButton.addEventLister("click", e => {
+submitButton.addEventListener("click", e => {
     e.preventDefault();
     addBookToLibrary();
     updateDisplay();
 });
+
+function removeBook(event) {
+    let targetBook = event.currentTarget.parentNode.parentNode.parentNode.dataset.bookID;
+    let index = myLibrary.findIndex(book => book.id == targetBook);
+    myLibrary.splice(index, 1);
+    updateDisplay();
+}
+
+function updateRead(event) {
+    let targetBook = event.currentTarget.parentNode.parentNode.parentNode.dataset.bookID;
+    let index = myLibrary.findIndex(book => book.id == targetBook);
+    myLibrary.splice(index, 1);
+    myLibrary[index].read = !myLibrary[index].read;
+}
